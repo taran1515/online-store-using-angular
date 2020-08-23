258fa3cdc85f9f68449143da26aec581c18bc52e
taran1515 <singhtaran1515@gmail.com>
2020-08-24 00:54:14 +0530

    Added order creation form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,8 +10,26 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items; 
+  checkoutForm;
 
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+    ) {
+      this.checkoutForm = this.formBuilder.group({
+        name:'',
+        address:''
+      });
+  }
+
+  onSubmit(customerData){
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    window.alert('Thank you! Your order has been submiitted')
+  }
+
+    
 
   
   
